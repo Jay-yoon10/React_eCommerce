464b3fdbec899720a826428581649f9557c23bbf
jay-yoon10 <yeojoon.yoon10@gmail.com>
2022-12-22 22:29:44 +1100

    updating directory page and category items page

---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,12 @@
+import './Directory.scss';
+import React from 'react';
+import CategoryItem from '../category-item/CategoryItem';
+export default function Directory({ categories }) {
+  return (
+    <div className='directory-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+}
